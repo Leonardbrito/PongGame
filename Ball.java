a1e272355bd94922cfc52398af693c2120dfe8fc
Leonardo <133671707+Leonardbrito@users.noreply.github.com>
2023-12-14 18:09:08 -0300

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -7,7 +7,12 @@ public class Ball extends Rectangle{
 	int xVelocity;
 	int yVelocity;
 	int initialSpeed = 2;
-	
+	/*
+     	O construtor recebe coordenadas x, y e dimensões width e height da bola.
+    	Um objeto Random é criado para gerar números aleatórios.
+    	randomXDirection e randomYDirection obtêm valores aleatórios entre 0 e 1, e são ajustados para -1 ou 1, determinando a direção inicial da bola.
+    	setXDirection e setYDirection são chamados para definir as direções iniciais da bola com base nos valores aleatórios.
+ 	*/
 	Ball(int x, int y, int width, int height){
 		super(x,y,width,height);
 		random = new Random();
@@ -22,13 +27,14 @@ public class Ball extends Rectangle{
 		setYDirection(randomYDirection*initialSpeed);
 		
 	}
-	
+	//configura as direcoes da bola
 	public void setXDirection(int randomXDirection) {
 		xVelocity = randomXDirection;
 	}
 	public void setYDirection(int randomYDirection) {
 		yVelocity = randomYDirection;
 	}
+	//atualiza as coordenadas da bola com base nas direções
 	public void move() {
 		x += xVelocity;
 		y += yVelocity;
